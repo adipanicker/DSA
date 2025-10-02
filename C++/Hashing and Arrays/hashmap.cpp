6443d9f8484bd5d2691867984300b84429b9b973
Aditya Panicker <adipanicker7@gmail.com>
2025-10-02 22:51:54 +0530

    update

--- C++/Hashing and Arrays/hashmap.cpp
+++ C++/Hashing and Arrays/hashmap.cpp
@@ -18,8 +18,8 @@ int main(){
     //     cout << x.first << "->" << x.second << endl; 
     // }
 
-    int q=3;
-    // cin >> q;
+    int q;
+    cin >> q;
     int num;
     while(q--){
         cin >> num;
